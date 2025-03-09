9b333461e628c9d9466e49fc3be3b10dac9f6b99
santhoshkumar0918 <krishnanramalingam87@gmail.com>
2025-03-09 17:50:21 +0530

    added

---
+++ lib/utils/Providers.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query/build/modern";
+import { WagmiProvider } from "wagmi";
+import { config } from "@/config";
+import { ReactNode } from "react";
+
+const queryClient = new QueryClient();
+
+interface ProvidersProps {
+  children: ReactNode;
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+}
